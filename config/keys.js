428db18f11630a51d62d9ff71f3426bceff18bb2
rranjanprajapati <78759321+rranjanprajapati@users.noreply.github.com>
2021-02-09 02:28:34 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI : 'mongodb+srv://thecreatix:Rrp@!Creatix@!7766@cluster0.aa2yh.mongodb.net/mern?retryWrites=true&w=majority',
+    mongoURI : 'mongodb+srv://thecreatix:password@cluster0.aa2yh.mongodb.net/mern?retryWrites=true&w=majority',
     secret : 'yoursecret'
 };
